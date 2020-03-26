35adf0e6009ee1c911a7a13ac4f5147d84df690f
Edwin Jarosiński <ejarosinski@e-point.pl>
2020-03-26 18:35:09 +0100

    Init commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {FunctionComponent} from "react";
+
+const App: FunctionComponent<{}> = () => {
+    return (
+        <div className="app">
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default App;
