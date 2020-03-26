938f1493f17b7be7f980baf3e7300955669e9910
Edwin Jarosiński <ejarosinski@e-point.pl>
2020-03-26 20:05:32 +0100

    axios basics

--- src/components/task1/List.tsx
+++ src/components/task1/List.tsx
@@ -1,7 +1,14 @@
 import {ListElement} from "./ListElement";
 import * as React from "react";
+import axios from "axios";
 
 export const List = () => {
+
+    axios.get("https://restcountries-v1.p.rapidapi.com/all", {
+        headers: {"x-rapidapi-key": "8a387081fdmsh16108e3f20fbbb2p19d786jsn00c7da8e2a84"}
+    })
+        .then((response) => console.log(response));
+
     return <>
         <ListElement name={"Brazylia"}/>
         <ListElement name={"Polska"}/>
