428cba5a6b62b8beb37d9eff5d6a587696b35d56
Edwin Jarosiński <ejarosinski@e-point.pl>
2020-03-27 08:36:14 +0100

    axios

--- src/components/task1/List.tsx
+++ src/components/task1/List.tsx
@@ -1,14 +1,24 @@
 import {ListElement} from "./ListElement";
 import * as React from "react";
 import axios from "axios";
+import {useEffect, useState} from "react";
 
 export const List = () => {
 
-    const list = ["Brazylia", "Polska", "Grecja"];
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+       axios.get("https://restcountries-v1.p.rapidapi.com/all", {
+           headers: {"x-rapidapi-key": "8a387081fdmsh16108e3f20fbbb2p19d786jsn00c7da8e2a84"}
+       })
+           .then(value => setList(value.data));
+    }, []);
+
+    console.log(list);
 
     return <>
         {list.map(country => {
-            return <ListElement name={country}/>
+            return <ListElement name={country.name}/>
         })}
         </>
 };
